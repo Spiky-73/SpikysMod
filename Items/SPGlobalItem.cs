f60076dca78be5473699bbd02c6912c5b05ab64a
Spiky-73 <cocojeanne73@gmail.com>
2021-11-06 11:16:22 +0100

    Equiq held item and improved watches

--- Items/SPGlobalItem.cs
+++ Items/SPGlobalItem.cs
@@ -1,37 +1,92 @@
 
 using Terraria;
 using Terraria.ModLoader;
+using Terraria.ID;
+using System.Collections.Generic;
 
-namespace SPYM {
-    
-    public class SPGlobalItem : GlobalItem {
-        public override void SetDefaults(Item item){
+using Terraria.UI;
 
-        }
+namespace SPYM.Items {
 
-        public override void OnConsumeItem(Item item, Player player) {
-            ClientConfig config = ModContent.GetInstance<ClientConfig>();
-            if(config.smartConsume){
-                Item smallestStack = Utility.SmallestStack(item, player, true);
-                if(smallestStack == null) return;
-                item.stack++;
-                smallestStack.stack--;
-            }
-        }
-        
-        public override void OnConsumeAmmo(Item ammo, Player player) {
-            ClientConfig config = ModContent.GetInstance<ClientConfig>();
-            if(config.smartAmmo && ammo.consumable){
-                Item smallestAmmo = Utility.SmallestStack(ammo, player, true);
-                if(smallestAmmo == null) return;
-                ammo.stack++;
-                smallestAmmo.stack--;
-            }
-        }
+	public class SPGlobalItem : GlobalItem {
 
-        public static bool Equipable(Item item){
-            return item.headSlot > 0 || item.bodySlot > 0 || item.legSlot > 0 || item.accessory || Main.projHook[item.shoot] || item.mountType != -1 || (item.buffType > 0 && (Main.lightPet[item.buffType] || Main.vanityPet[item.buffType]));
-        }
-    }
+		public override void SetDefaults(Item item) {
+			
+			switch (item.type) {
+			case ItemID.GoldWatch:
+			case ItemID.PlatinumWatch:
+				item.autoReuse = true;
+				item.useTurn = true;
+				item.useStyle = ItemUseStyleID.HoldingUp;
+				item.useTime = 2;
+				item.useAnimation = 2;
+				break;
+			}
+		}
+
+		public override bool UseItem(Item item, Player player) {
+			switch (item.type) {
+			case ItemID.GoldWatch:
+			case ItemID.PlatinumWatch:
+				Main.time += 28;  // 30-2
+				return true;
+			}
+			return base.UseItem(item, player);
+		}
+
+		public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
+			List<string> lines = new List<string>();
+			switch (item.type) {
+			case ItemID.GoldWatch:
+			case ItemID.PlatinumWatch:
+				lines.Add(SPTooltip.Watch);
+				AddTooltip(tooltips, lines);
+				break;
+			}
+		}
+
+		private void AddTooltip(List<TooltipLine> tooltips, List<string> toAdd) {
+			TooltipLine line;
+			int startIndex = tooltips.Count;
+			int startTooltip = 0;
+			for (int i = tooltips.Count - 1; i >= 0; i--) {
+				if (tooltips[i].Name.Length < 7) continue;
+				string st = tooltips[i].Name.Substring(7);
+				if (st != "Tooltip") continue;
+				if (!int.TryParse(st, out startTooltip)) continue;
+				startTooltip++;
+				startIndex = i + 1;
+				break;
+			}
+			for (int i = 0; i < toAdd.Count; i++) {
+				line = new TooltipLine(mod, $"Tooltip{startTooltip+ i}", toAdd[i]);
+				tooltips.Insert(startIndex+i, line);
+			}
+
+		}
+		public override void OnConsumeItem(Item item, Player player) {
+			ClientConfig config = ModContent.GetInstance<ClientConfig>();
+			if(config.smartConsume){
+				Item smallestStack = Utility.SmallestStack(item, player, true);
+				if(smallestStack == null) return;
+				item.stack++;
+				smallestStack.stack--;
+			}
+		}
+		
+		public override void OnConsumeAmmo(Item ammo, Player player) {
+			ClientConfig config = ModContent.GetInstance<ClientConfig>();
+			if(config.smartAmmo && ammo.consumable){
+				Item smallestAmmo = Utility.SmallestStack(ammo, player, true);
+				if(smallestAmmo == null) return;
+				ammo.stack++;
+				smallestAmmo.stack--;
+			}
+		}
+
+		public bool IsEquipable(Item item){
+			return item.headSlot > 0 || item.bodySlot > 0 || item.legSlot > 0 || item.accessory || Main.projHook[item.shoot] || item.mountType != -1 || (item.buffType > 0 && (Main.lightPet[item.buffType] || Main.vanityPet[item.buffType]));
+		}
+	}
 }
 
--- SPPlayer.cs
+++ SPPlayer.cs
@@ -45,20 +45,16 @@ namespace SPYM {
             adrenaline = false; // Needs to be here because of update order
         }
         public override void ProcessTriggers(TriggersSet triggersSet){
-
+            
+            if(Main.mouseRight && Main.mouseRightRelease
+                    && player.selectedItem < 10) { // Potential issue for accesories having a right click action
+                ItemSlot.SwapEquip(player.inventory, 0, player.selectedItem);
+			}
             if(SPYM.FavoritedBuff.JustPressed){
-                Item[] inv = player.inventory;
-                bool[] changed = new bool[player.inventory.Length-1];
-                for (int i = 0; i < player.inventory.Length-1; i++){
-                    if(!inv[i].favorited && inv[i].stack > 0) inv[i].stack *= -1;;
-                }
-                player.QuickBuff();
-                for (int i = 0; i < inv.Length-1; i++){
-                    if(!inv[i].favorited && inv[i].stack < 0) inv[i].stack *= -1;;
-                }
+                FavoritedBuff();
             }
 
-            if(Main.playerInventory && Main.mouseItem.type != 0){
+            if(Main.playerInventory && !Main.mouseItem.IsAir){
                 if(triggersSet.Hotbar1) {
                     TrySwapHeld(0);
                 }
@@ -94,17 +90,28 @@ namespace SPYM {
         }
 
         private void TrySwapHeld(int itemIndex){
-            if(!releasedSwap) return;
-            releasedSwap = false;
+			if (!releasedSwap) return;
+			releasedSwap = false;
             Item temp = player.inventory[itemIndex];
             player.inventory[itemIndex] = player.HeldItem;
             for (int i = player.inventory.Length - 1; i >= 0 ; i--){
-                if(player.inventory[i].IsAir) continue;
+                if(!player.inventory[i].IsAir) continue;
                 player.inventory[i] = temp;
                 Main.mouseItem.TurnToAir();
                 return;
             }
             Main.mouseItem = temp;
+        }
+
+        private void FavoritedBuff() {
+            Item[] inv = player.inventory;
+            for (int i = 0; i < player.inventory.Length - 1; i++) {
+                if (!inv[i].favorited && inv[i].stack > 0) inv[i].stack *= -1;
+            }
+            player.QuickBuff();
+            for (int i = 0; i < inv.Length - 1; i++) {
+                if (!inv[i].favorited && inv[i].stack < 0) inv[i].stack *= -1;
+            }
         }
     }
 
